6da774a2cf3e0826f6ad47183ccda438601da7e7
David Luna de León <davidlunadeleon@gmail.com>
2021-03-27 21:25:14 +0100

    Add function for quit button in HUD

--- src/scenes/HUD/HUD.gd
+++ src/scenes/HUD/HUD.gd
@@ -1,16 +1,14 @@
 extends CanvasLayer
 
-
 # Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	$QuitButton.connect("pressed", self, "_on_QuitButton_pressed")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
