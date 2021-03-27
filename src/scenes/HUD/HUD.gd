45ab2e7477edf998310d4cffe90883fc6ad37b15
David Luna de León <davidlunadeleon@gmail.com>
2021-03-27 23:31:57 +0100

    Add Main scene script and HUD solve alert
    - Add alert when HUD solve button is pressed
    - Add script for Main scene

--- src/scenes/HUD/HUD.gd
+++ src/scenes/HUD/HUD.gd
@@ -1,10 +1,13 @@
 extends CanvasLayer
 
+signal solve
+
 # Declare member variables here. Examples:
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$QuitButton.connect("pressed", self, "_on_QuitButton_pressed")
+	$SolveButton.connect("pressed", self, "_on_SolveButton_pressed")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -12,3 +15,6 @@ func _ready():
 
 func _on_QuitButton_pressed():
 	get_tree().quit()
+
+func _on_SolveButton_pressed():
+	emit_signal("solve")
---
+++ src/scenes/Main/Main.gd
@@ -0,0 +1,11 @@
+extends Node
+
+# Declare member variables here. Examples:
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
