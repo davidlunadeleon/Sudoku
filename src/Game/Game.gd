1acfa29c41a43fc4a2f831bc2191b6cfdd5db672
David Luna de León <davidlunadeleon@gmail.com>
2021-03-27 19:08:54 +0100

    Add tile selection in Game scene
    - Transform from mouse position to tile index in matrix
    - Clear tile when one tile is selected and a new one is clicked
    - Change border color when a tile is selected

--- src/Game/Game.gd
+++ src/Game/Game.gd
@@ -4,11 +4,15 @@ extends Node2D
 const INITIAL_DOMAIN = [1,2,3,4,5,6,7,8,9]
 const NROWS = 9
 const NCOLS = 9
+var tmap_transform
 var grid
+var selected_tile
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	grid = []
+	tmap_transform = $Grid_TileMap.get_transform()
+	selected_tile = Vector2()
 	clear_grid()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -29,10 +33,23 @@ func clear_grid():
 
 func _input(event):
 	if event is InputEventMouseButton:
-		if event.button_index == BUTTON_LEFT:
+		if event.button_index == BUTTON_LEFT && event.pressed:
+			clear_select()
 			var mouse_pos = get_viewport().get_mouse_position()
-			pos_to_cell_index(mouse_pos)
+			var tile_pos = pos_to_cell_index(mouse_pos)
+			if(tile_pos.x >= 0 && tile_pos.y >= 0):
+				$Grid_TileMap.set_cell(tile_pos.x, tile_pos.y, 9)
+				selected_tile = tile_pos
 
 func pos_to_cell_index(mouse_pos):
 	if mouse_pos.x < 1024 && mouse_pos.y < 1024:
-		print (mouse_pos)
+		mouse_pos.x = mouse_pos.x / tmap_transform.x.x
+		mouse_pos.y = mouse_pos.y / tmap_transform.y.y
+		var cell_pos = $Grid_TileMap.world_to_map(mouse_pos)
+		return Vector2(cell_pos.x, cell_pos.y)
+	else:
+		return Vector2(-1.0, -1.0)
+
+func clear_select():
+	$Grid_TileMap.set_cell(selected_tile.x, selected_tile.y, -1)
+	selected_tile = Vector2()
