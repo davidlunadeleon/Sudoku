90983386c5a7a774f2512124492876928068e411
David Luna de León <davidlunadeleon@gmail.com>
2021-03-28 12:55:08 +0200

    Add AC3 algorithm and solving function to Sudoku
    - Fix error when establishing constraints
    - Fix the passing of arrays by reference, use duplicate instead
    - Remove debugging print function calls
    - Add function get_grid
    - Implement AC3 Algorithm to establish arc consistency
    - Implement backtracking solution with MRV heuristic
    - Add solve function to try to solve the puzzle

--- src/scripts/Sudoku.gd
+++ src/scripts/Sudoku.gd
@@ -2,7 +2,6 @@ extends Node
 
 class_name Sudoku
 
-# Declare member variables here. Examples:
 const INITIAL_DOMAIN = [1,2,3,4,5,6,7,8,9]
 const SAMPLE_ROW = [0,0,0,0,0,0,0,0,0]
 const NROWS = 9
@@ -25,9 +24,9 @@ func init():
 	grid.clear()
 	sorted_cells.clear()
 	for x in range(NROWS):
-		grid.push_back(SAMPLE_ROW)
+		grid.push_back(SAMPLE_ROW.duplicate())
 		for y in range(NCOLS):
-			domains[str(x) + str(y)] = INITIAL_DOMAIN
+			domains[str(x) + str(y)] = INITIAL_DOMAIN.duplicate()
 
 func init_constraints():
 	for key in domains.keys():
@@ -35,10 +34,10 @@ func init_constraints():
 		var row = key[0].to_int()
 		var col = key[1].to_int()
 		for x in range(NCOLS):
-			if x != row:
+			if x != col:
 				constraints.get(key).push_back(str(row) + str(x))
 		for x in range(NROWS):
-			if x != col:
+			if x != row:
 				constraints.get(key).push_back(str(x) + str(col))
 		var sqr_row = row / SQR_SIZE
 		var sqr_col = col / SQR_SIZE
@@ -51,12 +50,95 @@ func init_constraints():
 func set_cell(pos, val):
 	grid[pos.x][pos.y] = val
 	domains[str(pos.x) + str(pos.y)] = [val]
-	print(pos)
-	print(domains.get(str(pos.x) + str(pos.y)))
-	print(constraints.get(str(pos.x) + str(pos.y)))
-	print(val)
 
 func clear_cell(pos):
 	grid[pos.x][pos.y] = 0
-	domains[str(pos.x) + str(pos.y)] = INITIAL_DOMAIN
-	print(pos)
+	domains[str(pos.x) + str(pos.y)] = INITIAL_DOMAIN.duplicate()
+
+func get_grid():
+	return grid
+
+func remove_inconsistencies(key1, key2):
+	var domain2 = domains.get(key2)
+	for x in domains.get(key1):
+		if domain2.size() == 1 && domain2.has(x):
+			domains.get(key1).erase(x)
+			return true
+	return false
+
+func ac3_algorithm():
+	var key_queue = []
+	for key in domains.keys():
+		for neighbor in constraints.get(key):
+			key_queue.push_back([key, neighbor])
+	while !key_queue.empty():
+		var key_arr = key_queue.pop_back()
+		var key1 = key_arr[0]
+		var key2 = key_arr[1]
+		if remove_inconsistencies(key1, key2):
+			for neighbor in constraints.get(key1):
+				key_queue.push_back([neighbor, key1])
+	for domain in domains.values():
+		if domain.empty():
+			return false
+	return true
+
+class Sorter:
+	static func sort_mrv(x, y):
+		var dom_x_size = x.values().front().size()
+		var dom_y_size = y.values().front().size()
+		return dom_x_size < dom_y_size
+
+func sort_cells():
+	var cells = []
+	for key in domains.keys():
+		cells.push_back({key: domains.get(key)})
+	cells.sort_custom(Sorter, "sort_mrv")
+	for cell in cells:
+		if cell.values().front().size() > 1:
+			sorted_cells.push_back(cell.keys().front())
+
+func is_var_consistent(key):
+	if domains.get(key).size() > 1:
+		return false
+	var val = domains.get(key).front()
+	for neighbor in constraints.get(key):
+		var domain = domains.get(neighbor)
+		if domain.empty() || (domain.size() == 1 && domain.has(val)):
+			return false
+	return true
+
+func backtrack(depth):
+	if depth >= sorted_cells.size():
+		return true;
+	var key = sorted_cells[depth]
+	var domain = domains.get(key).duplicate()
+	for val in domain:
+		domains[key] = [val]
+		var wasErased = []
+		for neighbor in constraints.get(key):
+			if domains.get(neighbor).has(val):
+				domains.get(neighbor).erase(val)
+				wasErased.push_back(true)
+			else:
+				wasErased.push_back(false)
+		if is_var_consistent(key) && backtrack(depth + 1):
+			return true
+		var i = 0
+		for neighbor in constraints.get(key):
+			if wasErased[i]:
+				domains.get(neighbor).push_back(val)
+			i = i + 1
+	domains[key] = domain
+	return false
+
+func solve():
+	if ac3_algorithm():
+		sort_cells()
+		if backtrack(0):
+			for key in domains.keys():
+				var x = key[0].to_int()
+				var y = key[1].to_int()
+				grid[x][y] = domains.get(key).front()
+			return true
+	return false
